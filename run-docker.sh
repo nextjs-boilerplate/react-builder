119760ad38c8691c414ae3fc3cab9359966cef42
林彦君 <josh.lin@pisofttech.com>
2017-10-10 15:26:35 +0800

    端口 去掉crossenv

--- run-docker.sh
+++ run-docker.sh
@@ -4,4 +4,4 @@ npm run build
 docker build . -t reactbuilder
 
 #run
-docker run -it -p 3003-3005:3003-3005 --restart=always  reactbuilder
+docker run -it -p 3003-3005:4033-4035 --restart=always  reactbuilder
